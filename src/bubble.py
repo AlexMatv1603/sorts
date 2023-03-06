f58983bcc6b4261149171773921504e56c4b803c
Alexander <alexmatv1603@gmail.com>
2023-03-06 17:23:35 +0300

    Fix bubble.py

--- src/bubble.py
+++ src/bubble.py
@@ -4,11 +4,12 @@ from src.utils.swap import swap_adjacent_numbers
 
 def bubble_sort(array: list):
     for j in range(len(array)):
-        for i in range(len(array) - 1 - j, -1, -1):
-            if i != len(array) - 1:
+        for i in range(len(array) - 1, j, -1):
+            if (i != 0) and (array[i - 1] > array[i]):
                 swap_adjacent_numbers(array, i)
 
 
-arr = [8, 3, 5, 1, 2, 4, 10, 7, 9]
+arr = [8, 3, 5, 1, 6, 2, 4, 10, 7, 9]
 
 bubble_sort(arr)
+print(arr)
